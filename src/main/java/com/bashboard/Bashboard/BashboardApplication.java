a8a8429ee7bece2c7e9573fc48e613ad9baac898
Jurica Kenda <jurica.kenda@gmail.com>
2019-08-26 20:45:33 +0200

    Initial commit

---
+++ src/main/java/com/bashboard/Bashboard/BashboardApplication.java
@@ -0,0 +1,13 @@
+package com.bashboard.Bashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BashboardApplication.class, args);
+	}
+
+}
