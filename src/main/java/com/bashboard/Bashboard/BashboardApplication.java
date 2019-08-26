a6149b8dbf39b9a287e3c61f80e60fc3338bcd03
Jurica Kenda <jurica.kenda@gmail.com>
2019-08-26 21:29:30 +0200

    <business> Adding the component scanning annotation

--- src/main/java/com/bashboard/Bashboard/BashboardApplication.java
+++ src/main/java/com/bashboard/Bashboard/BashboardApplication.java
@@ -2,8 +2,10 @@ package com.bashboard.Bashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.bashboard")
 public class BashboardApplication {
 
 	public static void main(String[] args) {
