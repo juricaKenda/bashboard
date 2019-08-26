9dae8104462e97cc556d06e9e0c811f4fbd88d1e
Jurica Kenda <jurica.kenda@gmail.com>
2019-08-26 21:30:28 +0200

    <model><business> Adding a page container data model

---
+++ src/main/java/com/bashboard/model/PageContainer.java
@@ -0,0 +1,22 @@
+package com.bashboard.model;
+
+import com.bashboard.model.PageContainer.PageContainerBuilder;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NonNull;
+import lombok.Value;
+
+@AllArgsConstructor
+@Builder
+@Value
+public class PageContainer {
+	@NonNull
+	private final String icon;
+	@NonNull
+	private final String link; 
+	@NonNull
+	private final String alias;
+	
+	
+}
