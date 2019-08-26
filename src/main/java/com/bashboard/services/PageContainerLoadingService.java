6a909b068ff2ad6a847d361fcfd259816c3a7911
Jurica Kenda <jurica.kenda@gmail.com>
2019-08-26 21:32:04 +0200

    <business> Adding a page container loading service

---
+++ src/main/java/com/bashboard/services/PageContainerLoadingService.java
@@ -0,0 +1,21 @@
+package com.bashboard.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bashboard.model.PageContainer;
+import com.bashboard.persistence.PageContainerRepository;
+
+@Service
+public class PageContainerLoadingService {
+
+	@Autowired
+	private PageContainerRepository pageContainerRepository;
+
+	public List<PageContainer> loadPageContainers() {
+		return pageContainerRepository.getAllPageContainers();
+	}
+	
+}
