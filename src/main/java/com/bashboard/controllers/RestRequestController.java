6c932db28a767db3dc86f5430e3875122b7c8b67
Jurica Kenda <jurica.kenda@gmail.com>
2019-08-27 20:49:23 +0200

    <business> Updating the controller so it uses a default delegation

--- src/main/java/com/bashboard/controllers/RestRequestController.java
+++ src/main/java/com/bashboard/controllers/RestRequestController.java
@@ -1,25 +1,30 @@
 package com.bashboard.controllers;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import com.bashboard.model.PageContainer;
+import com.bashboard.services.PageContainerDelegatingService;
 import com.bashboard.services.PageContainerLoadingService;
 
 @Controller
 public class RestRequestController {
 
 	@Autowired
-	private PageContainerLoadingService pageContainerLoadingService;
+	private PageContainerDelegatingService pageContainerDelegatingService;
+
 	
 	@GetMapping("/")
 	public String displayFavicon(Model model) {
-		List<PageContainer> pageContainers = pageContainerLoadingService.loadPageContainers();
-		model.addAttribute("pageContainers",pageContainers);
-		return "defaultClusters";
+		HashMap<String, List<PageContainer>> defaultClusters = pageContainerDelegatingService.getDefaultClusters();
+		model.addAttribute("centerClusterContainers",defaultClusters.get("centerClusterContainers"));
+		model.addAttribute("leftClusterContainers",defaultClusters.get("leftClusterContainers"));
+		model.addAttribute("rightClusterContainers",defaultClusters.get("rightClusterContainers"));
+		
+		return "defaultCluster";
 	}
 }
