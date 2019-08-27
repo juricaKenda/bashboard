2abbf9b1f814acfd20a25cddf354509643b08b42
Jurica Kenda <jurica.kenda@gmail.com>
2019-08-27 23:14:13 +0200

    <refactor> Removing unnecessary @Getter annotation

--- src/main/java/com/bashboard/model/PageContainer.java
+++ src/main/java/com/bashboard/model/PageContainer.java
@@ -4,13 +4,14 @@ import com.bashboard.model.PageContainer.PageContainerBuilder;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NonNull;
 import lombok.Value;
 
 @AllArgsConstructor
 @Builder
 @Value
-public class PageContainer {
+public class PageContainer{
 	@NonNull
 	private final String icon;
 	@NonNull
