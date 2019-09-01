9a463219b5091bf6b07a6a60100419a95a1cb916
Jurica Kenda <jurica.kenda@gmail.com>
2019-09-01 10:06:11 +0200

    <refactor> Removing InputContainer, using other domain objects instead

---
+++ src/main/java/com/bashboard/model/InputContainer.java
@@ -0,0 +1,13 @@
+package com.bashboard.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.Value;
+
+@NoArgsConstructor
+public class InputContainer {
+	@Setter
+	@Getter
+	private String text;
+}
