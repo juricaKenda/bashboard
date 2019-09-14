322ae030398f3d465c9ff352b310153d186c3305
Jurica Kenda <jurica.kenda@gmail.com>
2019-09-14 18:58:20 +0200

    <business> Adding additional logic to the 'open' command

--- src/main/java/com/bashboard/commandline/model/commands/OpenCommand.java
+++ src/main/java/com/bashboard/commandline/model/commands/OpenCommand.java
@@ -2,6 +2,13 @@ package com.bashboard.commandline.model.commands;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import com.bashboard.commandline.model.arguments.Argument;
+import com.bashboard.model.PageContainer;
+
+import lombok.NonNull;
 
 public class OpenCommand extends Command {
 
@@ -20,8 +27,19 @@ public class OpenCommand extends Command {
 
 	@Override
 	public String execute(Object input) {
-		String href = (String)input;
-		return "redirect:"+href;
+		List<PageContainer> containers = (List<PageContainer>)input;
+		return containers.stream()
+		.filter(makeRelevantPredicateChain(arguments).stream().reduce(each->true,Predicate::and))
+		.map(pageContainer->pageContainer.getLink())
+		.findFirst()
+		.orElse("default");
+	}
+	
+	private List<Predicate<PageContainer>> makeRelevantPredicateChain(List<Argument> arguments) {
+		return arguments.stream()
+				.map(argument -> (Argument)argument)
+				.map(argument->argument.getPredicate())
+				.collect(Collectors.toList());
 	}
 
 }
