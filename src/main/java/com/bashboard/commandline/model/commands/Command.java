b8ff861599ea6d6a3bade782969d85140e2b9dd9
Jurica Kenda <jurica.kenda@gmail.com>
2019-09-22 17:55:13 +0200

    <model> Changing visibility of command arguments

--- src/main/java/com/bashboard/commandline/model/commands/Command.java
+++ src/main/java/com/bashboard/commandline/model/commands/Command.java
@@ -6,11 +6,12 @@ import com.bashboard.commandline.model.arguments.Argument;
 
 
 public abstract class Command{
-	List<Argument> arguments;
+	public List<Argument> arguments;
 	public abstract boolean accepts(String argumentSignature);
 	abstract String getSignature();
 	public abstract Object execute(Object input);
 	public void addArguments(List<Argument> arguments) {
 		this.arguments = arguments;
 	}
+	
 }
